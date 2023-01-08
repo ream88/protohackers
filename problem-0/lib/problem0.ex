e257407a9c85fc7dfbebd393495cf12cf706f9ac
Mario Uher <uher.mario@gmail.com>
2023-01-08 10:18:25 +0100

    Add tests

--- problem-0/lib/problem0.ex
+++ problem-0/lib/problem0.ex
@@ -2,26 +2,38 @@ defmodule Problem0 do
   require Logger
 
   def start(port) do
-    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :raw, active: false, reuseaddr: true])
+    {:ok, socket} =
+      :gen_tcp.listen(port, mode: :binary, active: false, reuseaddr: true, exit_on_close: false)
+
     Logger.info("Accepting connections on port #{port}")
     loop(socket)
   end
 
   defp loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    {:ok, pid} = Task.Supervisor.start_child(Problem0.TaskSupervisor, fn -> serve(client) end)
+
+    {:ok, pid} =
+      Task.Supervisor.start_child(Problem0.TaskSupervisor, fn ->
+        Logger.info("Client connected")
+        serve(client)
+        :gen_tcp.close(client)
+      end)
+
     :ok = :gen_tcp.controlling_process(client, pid)
     loop(socket)
   end
 
   defp serve(socket) do
-    case :gen_tcp.recv(socket, 0) do
-      {:ok, data} ->
-        :gen_tcp.send(socket, data)
-        serve(socket)
+    {:ok, data} = recv_until_closed(socket, [])
+    Logger.info("Sending data back to client")
+    :gen_tcp.send(socket, data)
+  end
 
-      {:error, error} ->
-        Logger.warning("Error #{inspect(error)}")
+  defp recv_until_closed(socket, buffer) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} -> recv_until_closed(socket, [buffer, data])
+      {:error, :closed} -> {:ok, buffer}
+      {:error, reason} -> {:error, reason}
     end
   end
 end
--- problem-0/test/problem0_test.exs
+++ problem-0/test/problem0_test.exs
@@ -1,8 +1,17 @@
 defmodule Problem0Test do
   use ExUnit.Case
-  doctest Problem0
 
-  test "greets the world" do
-    assert Problem0.hello() == :world
+  setup do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+    {:ok, port: port}
+  end
+
+  test "echoes everything back", %{port: port} do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", port, mode: :binary, active: false)
+    assert :gen_tcp.send(socket, "foo") == :ok
+    assert :gen_tcp.send(socket, "bar") == :ok
+
+    :gen_tcp.shutdown(socket, :write)
+    assert :gen_tcp.recv(socket, 0) == {:ok, "foobar"}
   end
 end
